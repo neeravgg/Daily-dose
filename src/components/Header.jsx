694b5690bc86b2e93b2144fc8b9b013b7141ffdc
Neerav Garg <neeravgarg366@gmail.com>
2023-02-20 18:19:48 +0530

    third

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,12 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import { Menu, MenuItem, styled } from "@mui/material";
+import Hamburger from "./Hamburger";
+import { Menu, MenuItem, styled, Typography, MenuIcon } from "@mui/material";
 
 export default function MenuAppBar() {
-  const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleMenu = (event) => {
@@ -22,55 +20,54 @@ export default function MenuAppBar() {
   const ToolBox = styled(Toolbar)({
     justifyContent: "space-between",
   });
+  const Logotext = styled(Typography)`
+    font-size: 19px;
+    margin-left: 1px;
+    color: #000000;
+    font-weight: bold;
+  `;
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='static'>
+      <AppBar position='static' style={{ background: "#ffffff" }}>
         <ToolBox>
-          <div justifyContent='center'>
-            <IconButton
-              size='large'
-              edge='start'
-              color='#000000'
-              aria-label='menu'
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
+          {/* Hamburger icon */}
+          <Hamburger />
 
+          {/* Logo */}
+          <Box sx={{ display: "inline-flex", alignItems: "center" }}>
             <img height={40} src='/logo192.png' />
-          </div>
-          {auth && (
-            <div>
-              <IconButton
-                size='large'
-                aria-label='account of current user'
-                aria-controls='menu-appbar'
-                aria-haspopup='true'
-                onClick={handleMenu}
-                color='#000000'
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id='menu-appbar'
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          )}
+            <Logotext>Daily Dose</Logotext>
+          </Box>
+          {/* Search */}
+          <IconButton
+            size='large'
+            aria-label='account of current user'
+            aria-controls='menu-appbar'
+            aria-haspopup='true'
+            color='#000000'
+          >
+            <AccountCircle />
+          </IconButton>
+
+          <Menu
+            id='menu-appbar'
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+          </Menu>
         </ToolBox>
       </AppBar>
     </Box>
